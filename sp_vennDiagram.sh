75fb794b50aa587ff910dce3ad6035e265380658
Tong-Chen <chentong_biology@163.com>
2015-06-02 16:39:14 +0800

    Add num support FOR <sp_vennDiagram.sh>

--- sp_vennDiagram.sh
+++ sp_vennDiagram.sh
@@ -39,19 +39,43 @@ ${txtbld}OPTIONS${txtrst}:
 	-f	Data file (without header line, the first column is the
  		name of genes or otehr things you want to compare, the second
 		column is sample name, tab seperated)
-		${bldred}[NECESSARY, the type of venn depends on ]${txtrst}
+		${bldred}[NECESSARY]${txtrst}
+	-F	If you have the length of each set and the number
+		overlaps between or among each set,  pleass give TRUE here.
+		${bldred}[When this is TRUE, the value given to -f would be
+		the prefix for the output figure.]${txtrst}
+		
 	-a	The name for label1.
-		${bldred}[Necessary, one string in your second column,
-		ordered. ]${txtrst}
+		${bldred}[Necessary when -f is not FALSE, 
+		one string in your second column,ordered. ]${txtrst}
 	-b	The name for label1.
-		${bldred}[Necessary, one string in your second column,
+		${bldred}[Necessary when -f is not FALSE, 
+		one string in your second column,
 		ordered. A parameter to -b is enough for 2-way venn.]${txtrst}
 	-c	The name for label1.
-		${bldred}[Necessary, one string in your second column,
+		${bldred}[Necessary when -f is not FALSE, 
+		one string in your second column,
 		ordered. A parameter to -c is enough for 3-way venn.]${txtrst}
 	-d	The name for label1.
-		${bldred}[Necessary, one string in your second column],
+		${bldred}[Necessary when -f is not FALSE, 
+		one string in your second column],
 		ordered. A parameter to -d is enough for 4-way venn.]${txtrst}
+	-n	List of numbers for venn plot.
+
+		For two-set venn, the format is "100, 110, 50" represents (length_a, length_b,
+		a_b_overlap).  
+
+		For three-set venn, the format is "100, 110, 90, 50, 40, 40, 20" 
+		represents (length_a, length_b, length_c, 
+		a_b_overlap,  b_c_overlap, a_c_overlap, a_b_c_overlap).  
+
+	-l	List of labels for venn plot.
+
+		Format: "'a', 'b'" for two-set and "'a', 'b', 'c'" for three-set.
+
+		Pay attention to the order.
+		Both double-quotes and single-quotes are needed.
+
 	-C	Color for each area.
 		[${txtred}Ususlly the number of colors should
 		be equal to the number of labels. 
@@ -73,14 +97,22 @@ ${txtbld}OPTIONS${txtrst}:
 	-r	The resolution of output picture.[${txtred}Default 300 ppi${txtrst}]
 	-e	Execute or not[${bldred}Default TRUE${txtrst}]
 	-i	Install depended packages[${bldred}Default FALSE${txtrst}]
+
+Example:
+	s-plot vennDiagram -f prefix -F TRUE -n "120, 110, 50"  -l "'a','b'"
+	s-plot vennDiagram -f file -a h3k27ac -b ctcf
+	
 EOF
 }
 
 file=
+numGiven="FALSE"
 label1="CHENTONG"
 label2="CHENTONG"
 label3="CHENTONG"
 label4="CHENTONG"
+numList=
+labelList=
 execute='TRUE'
 ist='FALSE'
 uwid=20
@@ -90,7 +122,7 @@ ext='pdf'
 line_size=1
 color_v='"cornflowerblue", "green", "yellow", "darkorchid1"'
 
-while getopts "hf:a:b:c:d:C:w:u:r:e:E:i:" OPTION
+while getopts "hf:F:a:b:c:d:n:l:C:w:u:r:e:E:i:" OPTION
 do
 	case $OPTION in
 		h)
@@ -99,6 +131,9 @@ do
 			;;
 		f)
 			file=$OPTARG
+			;;
+		F)
+			numGiven=$OPTARG
 			;;
 		a)
 			label1=$OPTARG
@@ -111,6 +146,12 @@ do
 			;;
 		d)
 			label4=$OPTARG
+			;;
+		n)
+			numList=$OPTARG
+			;;
+		l)
+			labelList=$OPTARG
 			;;
 		C)
 			color_v=$OPTARG
@@ -156,69 +197,6 @@ if ($ist){
 }
 library(VennDiagram)
 
-data <- read.table(file="$file", sep="\t")
-
-num <- 0
-
-if("${label1}" != "CHENTONG"){
-	$label1 <- data[grepl("\\\\<${label1}\\\\>",data[,2]),1]
-	num <- num + 1
-}
-
-if("${label2}" != "CHENTONG"){
-	$label2 <- data[grepl("\\\\<${label2}\\\\>",data[,2]),1]
-	num <- num + 1
-}
-
-if("${label3}" != "CHENTONG"){
-	$label3 <- data[grepl("\\\\<${label3}\\\\>",data[,2]),1]
-	num <- num + 1
-}
-
-if("${label4}" != "CHENTONG"){
-	$label4 <- data[grepl("\\\\<${label4}\\\\>",data[,2]),1]
-	num <- num + 1
-}
-
-color_v <- c(${color_v})[1:num]
-
-if(num == 4){
-	p <- venn.diagram( 
-		x = list($label1=$label1, $label4=$label4, $label2=$label2,
-		$label3=$label3),
-		filename = NULL, col = "black", lwd = 1, 
-		fill = color_v,
-		alpha = 0.50,
-		label.col = c("orange", "white", "darkorchid4", "white", "white", "white", "white", "white", "darkblue", "white", "white", "white", "white", "darkgreen", "white"),
-		cex = 2.5, fontfamily = "Helvetica",
-		cat.col = color_v,cat.cex = 2.5,
-		cat.fontfamily = "Helvetica"
-	)
-} else if (num==3) {
-	p <- venn.diagram( 
-		x = list($label1=$label1, $label2=$label2, $label3=$label3),
-		filename = NULL, col = "transparent", 
-		fill = color_v,
-		alpha = 0.50,
-		label.col = c("white", "white", "white", "white", "white", "white", "white"),
-		cex = 2.5, fontfamily = "Helvetica", cat.default.pos="text",
-		cat.pos=0,  
-		cat.col = color_v,cat.cex = 2.5,cat.fontfamily = "Helvetica"
-	)
-} else if (num==2) {
-	p <- venn.diagram( 
-		x = list($label1=$label1, $label2=$label2),
-		filename = NULL, col = "transparent", 
-		fill = color_v,
-		alpha = 0.50,
-		label.col = c("white"),
-		cex = 2.5, fontfamily = "Helvetica", cat.default.pos="text",
-		cat.pos=0,  
-		cat.col = color_v,cat.cex = 2.5,cat.fontfamily = "Helvetica"
-	)
-}
-
-#cat.col = c("darkblue", "darkgreen", "orange", "darkorchid4"),
 
 if ("${ext}" == "png") {
 	png(filename="${file}${mid}.png", width=$uwid, height=$vhig,
@@ -233,10 +211,96 @@ if ("${ext}" == "png") {
 	print("This format is currently unsupported. Please check the file <Rplots.pdf> in current dirsctory.")
 }
 
-grid.draw(p)
+
+if (! ${numGiven}) {
+
+	data <- read.table(file="$file", sep="\t")
+
+	num <- 0
+
+	if("${label1}" != "CHENTONG"){
+		$label1 <- data[grepl("\\\\<${label1}\\\\>",data[,2]),1]
+		num <- num + 1
+	}
+
+	if("${label2}" != "CHENTONG"){
+		$label2 <- data[grepl("\\\\<${label2}\\\\>",data[,2]),1]
+		num <- num + 1
+	}
+
+	if("${label3}" != "CHENTONG"){
+		$label3 <- data[grepl("\\\\<${label3}\\\\>",data[,2]),1]
+		num <- num + 1
+	}
+
+	if("${label4}" != "CHENTONG"){
+		$label4 <- data[grepl("\\\\<${label4}\\\\>",data[,2]),1]
+		num <- num + 1
+	}
+
+	color_v <- c(${color_v})[1:num]
+
+	if(num == 4){
+		p <- venn.diagram( 
+			x = list($label1=$label1, $label4=$label4, $label2=$label2,
+			$label3=$label3),
+			filename = NULL, col = "black", lwd = 1, 
+			fill = color_v,
+			alpha = 0.50,
+			label.col = c("orange", "white", "darkorchid4", "white", "white", "white", "white", "white", "darkblue", "white", "white", "white", "white", "darkgreen", "white"),
+			cex = 2.5, fontfamily = "Helvetica",
+			cat.col = color_v,cat.cex = 2.5,
+			cat.fontfamily = "Helvetica"
+		)
+	} else if (num==3) {
+		p <- venn.diagram( 
+			x = list($label1=$label1, $label2=$label2, $label3=$label3),
+			filename = NULL, col = "transparent", 
+			fill = color_v,
+			alpha = 0.50,
+			label.col = c("white", "white", "white", "white", "white", "white", "white"),
+			cex = 2.5, fontfamily = "Helvetica", cat.default.pos="text",
+			cat.pos=0,  
+			cat.col = color_v,cat.cex = 2.5,cat.fontfamily = "Helvetica"
+		)
+	} else if (num==2) {
+		p <- venn.diagram( 
+			x = list($label1=$label1, $label2=$label2),
+			filename = NULL, col = "transparent", 
+			fill = color_v,
+			alpha = 0.50,
+			label.col = c("white"),
+			cex = 2.5, fontfamily = "Helvetica", cat.default.pos="text",
+			cat.pos=0,  
+			cat.col = color_v,cat.cex = 2.5,cat.fontfamily = "Helvetica"
+		)
+	}
+	grid.draw(p)
+} else {
+#---venn plot for given numbers---------
+	numList <- c(${numList})
+	labelList <- c(${labelList})
+
+	num <- length(labelList)
+	color_v <- c(${color_v})[1:num]
+	
+	if (num==2) {
+		draw.pairwise.venn(area1=numList[1], area2=numList[2],
+		cross.area=numList[3], category=labelList, lwd=rep(1,1),
+		lty=rep(2,2), col="transparent", fill=color_v,
+		cat.col=color_v)
+	} else if (num==3) {
+		draw.triple.venn(area1=numList[1], area2=numList[2],
+		area3=numList[3], n12=numList[4], n23=numList[5],
+		n13=numList[6], n123=numList[7], 
+		category=labelList, col="transparent", fill=color_v,
+		cat.col=color_v, reverse=FALSE)
+	}
+}
 
 dev.off()
 
+#cat.col = c("darkblue", "darkgreen", "orange", "darkorchid4"),
 #ggsave(p, filename="${file}${mid}.${ext}", dpi=$res, width=$uwid,
 #height=$vhig, units=c("cm"))
 
